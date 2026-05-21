b48d8addf8df438115dcf817b85ececbd4ad25b3
HoneySeekBee <dosik112290@gmail.com>
2026-05-22 08:11:28 +0900

    fix: start.bat exe 직접 실행으로 변경 (빠른 시작)

--- MSMES/start.bat
+++ MSMES/start.bat
@@ -3,5 +3,5 @@ title MSMES
 cd /d "%~dp0"
 taskkill /IM "MSMES.Web.exe" /F >nul 2>&1
 start "" cmd /c "timeout /t 4 >nul && start http://localhost:5000"
-dotnet run --project src\MSMES.Web\MSMES.Web.csproj --urls "http://localhost:5000"
+src\MSMES.Web\bin\Release\net8.0\MSMES.Web.exe --urls "http://localhost:5000"
 pause
